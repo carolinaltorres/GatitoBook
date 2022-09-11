3c71a4460c2ce9aa511f1d32deab7d3753b203c2
Carolina <carolinadelimatorres@gmail.com>
2022-09-11 16:34:01 -0300

    Using reactive forms

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -18,7 +18,8 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    MensagemModule
+    MensagemModule,
+    ReactiveFormsModule
   ],
   exports: [
     HomeComponent
